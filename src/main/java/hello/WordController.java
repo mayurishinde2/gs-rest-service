0c8e9cbac16aabee7bff1faedee6c3da4ad76c07
Mayuri <mayuris@mkcl.org>
2018-11-19 18:08:44 +0530

    Update WordController.java

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -1,8 +1,26 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import hello.Word;
+import hello.WordService;
 
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+	@GetMapping("/words/{word}")
+    public Blog show(@PathVariable String word){
+        return blogMockedData.getBlogById(word);
+    }
+	
+	@GetMapping(path = "/words/{word}", produces = "application/json;charset=UTF-8") 
+	@ResponseBody
+	public WordResponse getWordResponse(@PathVariable String word) {
+		Word wordResponse = null;
+		if(word.length() > 0 ){
+		private WordService wordService = new WordService();
+		wordResponse=wordService.getWordResponse()
+		return wordResponse;
+		}
+		return wordResponse;
+	}
 }
